5623d1c8255f781c2fcdeca4adbaca8e8ad22420
Maria Moraru <morarumaria1988@gmail.com>
2024-05-15 14:27:05 +0200

    Add project files.

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi
+{
+    class TodoDb : DbContext
+    {
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
